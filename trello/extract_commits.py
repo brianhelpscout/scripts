453721d8530d240f2f9aa10cf25b029bd3165e37
Brian Sensale <bsensale@Brians-MBP.fios-router.home>
2019-01-31 17:46:20 -0500

    Only pull from the Done column.\ Scan the actions array for more descriptive PR names

--- trello/extract_commits.py
+++ trello/extract_commits.py
@@ -2,18 +2,47 @@
 import argparse, json
 
 parser = argparse.ArgumentParser()
-parser.add_argument('input', type=argparse.FileType('r'))
+parser.add_argument('-i', '--input', type=argparse.FileType('r'))
 args = parser.parse_args()
 
-urls = []
 data = json.load(args.input)
-for card in data["cards"]:
-  for attachment in card["attachments"]:
-    if "github" in attachment["url"] and attachment["url"] not in urls:
-      urls.append(attachment["url"])
 
-output = open("output.csv", "w")
-for url in urls:
-  output.write(url+"\n")
+done_list = "Not Found"
+for list in data['lists']:
+  if 'Done' == list['name']:
+    done_list = list['id']
+    break;
+
+print("Done List ID: " + done_list)
+
+# Pull all the github urls from the cards themselves.
+urls = []
+for card in data['cards']:
+  if done_list != card['idList']:
+    continue;
+  for attachment in card['attachments']:
+    if 'github' in attachment['url'] and attachment['url'] not in urls:
+      urls.append(attachment['url'])
+
+# Scan the action list to see if the PR has a description, and append it.
+results = urls.copy()
+for action in data['actions']:
+  if (action['type'] == 'addAttachmentToCard' and
+        'url' in action['data']['attachment'].keys() and
+        'github' in action['data']['attachment']['url'] and
+        action['data']['attachment']['name'].startswith('#') and
+        action['data']['attachment']['url'] in urls):
+    attachment = action['data']['attachment']
+    url = attachment['url']
+    print(url)
+    pos = results.index(url)
+    results.pop(pos)
+    results.insert(pos, url + ' ' + attachment['name'])
+    urls.remove(url)
+
+# Write the results to the output file.
+output = open('output.csv', 'w')
+for result in results:
+  output.write(result+'\n')
 output.close()
 
